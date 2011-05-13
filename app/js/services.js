e8bce4205e7fcb60440e80b9b4c8e058f6ba18df
Vojta Jina <vojta@gemin-i.org>
2011-05-13 23:17:21 +0200

    Custom $resource service
    General solution for REST resource collection handling...

--- app/js/services.js
+++ app/js/services.js
@@ -109,3 +109,101 @@ angular.service('$projects', function($xhr) {
     return projects;
   };
 });
+
+/**
+ * RESOURCE factory service
+ * 
+ * Creates resource collection for given url
+ * @see ResourceCollection
+ * 
+ * @param {string} url Url for the collection
+ * @param {Object=} relations Relations of this resource (1-1, 1-N)
+ */
+angular.service('$resource', function($xhr) {
+  return function(url, relations) {
+    return new ResourceCollection($xhr, url, true, relations);
+  };
+});
+
+/**
+ * ResourceCollection represents a collection of resources
+ * 
+ * TODO(vojta) pagination
+ * 
+ * @param {Object} $xhr Angular's $xhr service 
+ * @param {string} url Url of the collection
+ * @param {boolean=} autoload Should auto load details of all resources ?
+ * @param {Object=} relations Configuration of relations
+ * @returns {ResourceCollection}
+ */
+function ResourceCollection($xhr, url, autoload, relations) {
+  this.$xhr = $xhr;
+  this.relations_ = relations;
+  this.items = [];
+  this.loadIndex(url, autoload);
+}
+
+ResourceCollection.prototype = {
+
+  /**
+   * Load index (array of urls)
+   * @param {string} url
+   * @param {boolean} autoload Should load all details when index loaded ?
+   */
+  loadIndex: function(url, autoload) {
+    var self = this;
+    this.$xhr('GET', url, function(code, response) {
+      self.items_ = response.items;
+      if (autoload) self.loadDetails();
+    });
+  },
+
+  /**
+   * Load details of all resources
+   * Fires xhr for every resource in index list
+   */
+  loadDetails: function() {
+    var self = this;
+    angular.forEach(this.items_, function(url, i) {
+      self.$xhr('GET', url, function(code, resource) {
+        self.loadRelations(resource);
+        self.items[i] = resource;
+      });
+    });
+  },
+
+  /**
+   * Load relations for given resource
+   * @param {Object} resource
+   */
+  loadRelations: function(resource) {
+    var self = this;
+    angular.forEach(this.relations_, function(type, name) {
+      if (type == ResourceCollection.RELATION.ONE) {
+        self.$xhr('GET', resource[name], function(code, relation) {
+          resource[name.charAt(0).toUpperCase() + name.slice(1)] = relation;
+        });
+      } else if (type == ResourceCollection.RELATION.MANY) {
+        resource[name.charAt(0).toUpperCase() + name.slice(1)] = new ResourceCollection(self.$xhr, resource[name]);
+      }
+    });
+  },
+
+  /**
+   * Number of resources in the collection
+   * 
+   * @returns {Number}
+   */
+  countTotal: function() {
+    return this.items_.length;
+  }
+};
+
+/**
+ * Possible relations
+ * @const
+ */
+ResourceCollection.RELATION = {
+  ONE: 1,
+  MANY: 2
+};
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -101,3 +101,65 @@ describe('$tickets', function() {
     expect(tickets[0].Comments.data).toEqual([comment1, comment2]);
   });
 });
+
+describe('$resource', function() {
+  var scope, xhr;
+  
+  function expectItems(items) {
+    xhr.expectGET('/url').respond({items: items});
+  }
+  
+  beforeEach(function() {
+    scope = angular.scope();
+    xhr = scope.$service('$browser').xhr;
+  });
+
+  it('should auto load details by default', function() {
+    expectItems(['/first', '/second', '/third']);
+    xhr.expectGET('/first').respond({id: '1'});
+    xhr.expectGET('/second').respond({id: '2'});
+    xhr.expectGET('/third').respond({id: '3'});
+
+    var rc = scope.$service('$resource')('/url');
+    xhr.flush();
+
+    expect(rc.items.length).toBe(3);
+    expect(rc.items[0].id).toEqual('1');
+    expect(rc.items[1].id).toEqual('2');
+    expect(rc.items[2].id).toEqual('3');
+  });
+  
+  it('countTotal() should return number of resources even before details are loaded', function() {
+    expectItems(['/first', '/second', '/third']);
+
+    var rc = new ResourceCollection(scope.$service('$xhr'), '/url');
+    xhr.flush();
+
+    expect(rc.items.length).toBe(0);
+    expect(rc.countTotal()).toBe(3);
+  });
+
+  it('should load 1-1 relations', function() {
+    expectItems(['/first']);
+    xhr.expectGET('/first').respond({id: '1', relation: '/rel-url'});
+    xhr.expectGET('/rel-url').respond({id: 'relation'});
+
+    var rc = scope.$service('$resource')('/url', {relation: ResourceCollection.RELATION.ONE});
+    xhr.flush();
+
+    expect(rc.items[0].Relation.id).toEqual('relation');
+  });
+
+  it('should create collections for 1-n relations', function() {
+    expectItems(['/first']);
+    xhr.expectGET('/first').respond({id: '1', relation: '/rel-url'});
+    xhr.expectGET('/rel-url').respond({items: ['/rel1', '/rel2']});
+
+    var rc = scope.$service('$resource')('/url', {relation: ResourceCollection.RELATION.MANY});
+    xhr.flush();
+
+    expect(rc.items[0].Relation).toBeDefined();
+    expect(rc.items[0].Relation instanceof ResourceCollection).toBe(true);
+    expect(rc.items[0].Relation.countTotal()).toEqual(2);
+  });
+});

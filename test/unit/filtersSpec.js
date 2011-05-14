54329b72976fe62861b53f76f0e39fa633999a88
Vojta Jina <vojta@gemin-i.org>
2011-05-14 23:21:05 +0200

    One more higher test for gmtdate filter

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -1,10 +1,14 @@
 describe('gmtdate', function() {
   beforeEach(function() {
-    spyOn(angular.filter, 'date');
+    spyOn(angular.filter, 'date').andCallThrough();
   });
 
   it('should convert gmt to iso extended format', function() {
     angular.filter.gmtdate('2011-05-01T20:06:21+0000', null);
     expect(angular.filter.date).toHaveBeenCalledWith('2011-05-01T20:06:21.000Z', null);
+  });
+
+  it('should format basic date', function() {
+    expect(angular.filter.gmtdate('2011-05-01T20:06:21+0000', 'd.M.yyyy')).toEqual('1.5.2011');
   });
 });

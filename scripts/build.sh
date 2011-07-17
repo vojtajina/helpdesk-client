aa89c40de3bc73ef9a2290ea72562ac69e4210c3
Vojta Jina <vojta.jina@gmail.com>
2011-07-18 00:02:20 +0200

    Update build script to copy partials and create zip archive

--- scripts/build.sh
+++ scripts/build.sh
@@ -24,10 +24,11 @@ done
 # compile minified js file
 java -jar "$SCRIPT_DIR/lib/closure/compiler.jar" --js $BUILD_FILE --js_output_file $MINIFIED_FILE
 
-# copy app.css, index.html, angular.js
-cp "$APP_DIR/css/app.css" "$BUILD_DIR/helpdesk.css" 
+# copy app.css, index.html, angular.js, partials
+cp "$APP_DIR/css/app.css" "$BUILD_DIR/helpdesk.css"
 cp "$APP_DIR/index.html" $BUILD_DIR
 cp "$APP_DIR/lib/angular/angular.min.js" $BUILD_DIR
+cp -r "$APP_DIR/partials" "$BUILD_DIR/"
 
 # change the index.html
 sed -Ei "s|css/app.css|helpdesk.css|" "$BUILD_DIR/index.html"
@@ -35,3 +36,5 @@ sed -Ei "s|lib/angular/angular.js|angular.min.js|" "$BUILD_DIR/index.html"
 sed -Ei "s|js/utils.js|helpdesk.min.js|" "$BUILD_DIR/index.html"
 sed -Ei "/script src=\"js/d" "$BUILD_DIR/index.html"
 
+# create archive
+tar -zcvf helpdesk-client.tar.gz "$BUILD_DIR"
